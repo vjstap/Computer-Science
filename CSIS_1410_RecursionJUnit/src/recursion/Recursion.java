1179e53740e5899341e5c8c25795fe050c44916a
vjs <vincentjstapley@gmail.com>
2025-02-26 02:01:31 -0700

    Test

--- CSIS_1410_RecursionJUnit/src/recursion/Recursion.java
+++ CSIS_1410_RecursionJUnit/src/recursion/Recursion.java
@@ -7,47 +7,69 @@ package recursion;
 public class Recursion {
 	/**
 	 * Returns the sum of each of the digits, also makes the output non-negative
+	 * 
 	 * @param n digit that is being split into each individual digit that comprises it
 	 * @return returns total of every digit in n
 	 */
 	public static int sumOfDigits(int n) {
-		int total =  0;
-		if(n < 0) {
+		int total = 0;
+		if (n < 0) {
 			n = n * -1;
 		}
-		while(n > 0) {
+		while (n > 0) {
 			total += (n % 10);
 			n /= 10;
 		}
 		return total;
 	}
+	/**
+	 * Returns the original string with a space in between each character, including spaces
+	 * @param str string input spaces will be appended into
+	 * @return returns the string with " " between each character
+	 */
 	public static String toUpper(String str) {
-		str.toUpperCase();
-		for(int i = 0; i < str.length(); i++) {
-			
+		str = str.toUpperCase();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			sb.append(str.charAt(i));
+			if (i < str.length() - 1) {
+				sb.append(" ");
+			}
 		}
-		//TODO This method separates all characters by a space and changes all lowercase letters to uppercase letters.
+		return sb.toString();
 	}
+	/**
+	 * 
+	 * @param letters
+	 * @param index
+	 * @return
+	 */
 	public static int countSmiles(char[] letters, int index) {
-		//TODO This method counts the number of colons followed by a closing parenthesis starting at the specified index.
-		//An empty character array should return 0.
+		if (letters == null || letters.length < 2 || index < 0 || index >= letters.length - 1) {
+			return 0;
+		}
+
+		int count = 0;
+		for (int i = index; i < letters.length - 1; i++) {
+			if (letters[i] == ':' && letters[i + 1] == ')') {
+				count++;
+			}
+		}
+
+		return count;
 	}
+
 	public static double harmonic(int n) {
-		//TODO
-		/*
-		 * If n is positive, the method should return the n-th harmonic number, which is calculated like this:  1/1 + 1/2 + 1/3 + ... + 1/n.
-		 *	If n is negative, the method should return the additive inverse of the n-th harmonic number.
-		 *	If n is zero, an IllegalArgumentException should be thrown.
-		 */
-		if(n == 0) {
+		if (n == 0) {
 			throw new IllegalArgumentException("The argument n can't be zero.");
 		}
-		if(n == 1) {
+		if (n == 1) {
 			return 1;
-		} else if(n < 0) {
+		} else if (n < 0) {
 			return -1 * harmonic(-n);
 		} else {
-			return 1.0/n + harmonic(n-1);
+			return 1.0 / n + harmonic(n - 1);
 		}
 	}
 }
--- CSIS_1410_RecursionJUnit/test/recursion/HarmonicTest.java
+++ CSIS_1410_RecursionJUnit/test/recursion/HarmonicTest.java
@@ -5,10 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class HarmonicTest {
+    private static final double DELTA = 1e-9;
 
 	@Test
-	void harmonic() {
-		fail("Not yet implemented");
+	void harmonic_PositiveNumber() {
+		assertEquals(1.0 + 0.5 + 0.333333333 + 0.25, Recursion.harmonic(4), DELTA);
 	}
-
+	@Test
+	void harmonic_NegativeNumber() {
+		assertEquals(-1.0 - 0.5 - 0.333333333 - 0.25, Recursion.harmonic(-4), DELTA);
+	}
+	@Test
+	void harmonic_One() {
+		assertEquals(1.0, Recursion.harmonic(1), DELTA);
+	}
+	@Test
+	void harmonic_Zero() {
+		assertThrows(IllegalArgumentException.class, () -> {Recursion.harmonic(0);});
+	}
+	@Test
+	void harmonic_BigNumber() {
+		assertEquals(1.0 + 0.5 + 0.333333333 + 0.25 + 0.2 + 0.166666667 + 0.142857143 + 0.125 + 0.111111111 + 0.1, Recursion.harmonic(10), DELTA);
+	}
 }
